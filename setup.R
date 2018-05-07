210be4d723f267a2e08b9e2e801db22c0c759123
Florian Huber <florian_huber@gmx.at>
2018-05-07 16:51:41 +0200

    Added masterfile, changed setup files/chunks, finished cleanup of 1_DataLoading_FeaturePreselect.Rmd

---
+++ setup.R
@@ -0,0 +1,21 @@
+knitr::opts_chunk$set(echo = T)
+knitr::opts_chunk$set(message = T)
+knitr::opts_chunk$set(warning = F)
+knitr::opts_chunk$set(error = T)
+knitr::opts_chunk$set(cache = T)
+Sys.setlocale("LC_ALL", "en_IE.UTF-8")
+
+ipak(tidyverse)
+ipak(mlr)
+ipak(parallelMap)
+ipak(parallel)
+
+#Custom functions
+walk(list.files("./R", pattern = "*.R", full.names = T), source)
+
+# dynamically assign number of processors
+if (system('hostname', intern = TRUE) == 'spinoza.embl.de') {
+   parallelStartMulticore(cpus = 10)
+} else {
+   parallelStartMulticore(cpus = detectCores() - 1)
+}
