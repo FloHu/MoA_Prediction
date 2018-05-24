12edfa535d7afc3534645ed27aab83c239ddb1d3
Florian Huber <florian_huber@gmx.at>
2018-05-24 20:12:57 +0200

    Restructure 1_DataLoading_FeaturePreselect.Rmd to accommodate different ways of constructing our drug-feature matrix and to add metadata

--- R/select_dosage.R
+++ R/select_dosage.R
@@ -1,8 +1,10 @@
-select_dosage <- function(dfr) {
-   if ( all(dfr$significant) || all(!dfr$significant) ) {
-      return(dfr[which.max(dfr$conc), ])
-   } else {
-      sel_conc <- max(dfr$conc[dfr$significant])
-      return(dfr[dfr$conc == sel_conc, ])
-   }
+select_dosage_most_ias <- function(dfr) {
+   subfr <- select_if(ungroup(dfr), is.logical) # need to ungroup!
+   sum_signif <- apply(subfr, 1, sum)
+   # first occurrence wins, meaning the highest dosage will have the maximum rank in the case of ties:
+   # (as dosages were sorted previously)
+   ranks <- rank(sum_signif, ties.method = "first")
+   ranks <- ranks == max(ranks)
+   dfr$selector <- ranks
+   return(dfr)
 }
