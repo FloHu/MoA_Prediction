91afa24adbbd9e058d21510f1048982a01334fb5
Florian Huber <florian_huber@gmx.at>
2018-05-30 09:54:49 +0200

    Added metadata_funcs.R for documentation purposes, going to replace by dfr with list columns

--- R/metadata_funcs.R
+++ R/metadata_funcs.R
@@ -1,29 +1,41 @@
-add_metadata <- function(drug_feature_matrix,
-                         drug_selection = c("not_specified", "all_drugs", "main_moa_drugs"),
-                         drug_classification = c("not_specified", "before_2018-05-09", "after_2018-05-09"),
-                         feature_selection = c("not_specified", "all", "top5pct", "top10pct", "top15pct", "top20pct", "top25pct", "top30pct", "top40pct", "top50pct"),
-                         dosage_selection = c("not_specified", "most_interactions"),
-                         datasets_included = NULL) {
-   # adds metadata to a matrix so that we can distinguish between:
-   # the drugs that were selected: attribute drug_selection [all, main_moa_drugs, ...]
-   # and similarly for the drug_classification used [before_2018-05-09, after_2018-05-09, ...]
-   # the feature_selection and transformation_procedure (if any) [top5pct, top10pct, ...]
-   # the dosage_selection procedure [most_interactions, ...]
-   # the datasets_included: currently one of nichols_2011, knime; to be extended later on
-   # the meanings of these metadata are documented in the notebook
-   drug_feature_matrix <- structure(drug_feature_matrix,
-                                    drug_selection = match.arg(drug_selection),
-                                    drug_classification = match.arg(drug_classification),
-                                    feature_selection = match.arg(feature_selection),
-                                    dosage_selection = match.arg(dosage_selection),
-                                    datasets_included = match.arg(datasets_included, choices = c("not_specified", "nichols_2011", "knime"), several.ok = TRUE),
-                                    date_created = Sys.Date())
-   return(drug_feature_matrix)
-}
+# functions to add metadata to a matrix so that we can distinguish between:
+# the feature_selection and transformation_procedure (if any) [top5pct, top10pct, ...]
+# the dosage_selection procedure [most_interactions, ...]
+# the datasets_included: currently one of nichols_2011, knime; to be extended later on
+annotate_list <-
+  function(l, add_metadata_func, ...) {
+    # take a list of objects to which to apply one of the metadata functions below
+    # ... takes the additional arguments for add_metadata_func
+    l <- lapply(l, add_metadata_func, ...)
+    return(l)
+  }
+
+add_metadata_featureselection <-
+  function(drug_feature_matrix, feature_selection = c("all", "top5pct", "top10pct", "top15pct", "top20pct", "top25pct", "top30pct", "top40pct", "top50pct"))
+  {
+    drug_feature_matrix <- structure(drug_feature_matrix,
+                                     feature_selection = match.arg(feature_selection))
+    return(drug_feature_matrix)
+  }
+
+add_metadata_dosage <-
+  function(drug_feature_matrix, dosage_selection = c("none", "most_interactions"))
+  {
+    drug_feature_matrix <- structure(drug_feature_matrix,
+                                     dosage_selection = match.arg(dosage_selection))
+    return(drug_feature_matrix)
+  }
+
+add_metadata_included_datasets <-
+function(drug_feature_matrix, datasets_included = c("nichols_2011", "knime"))
+  {
+    drug_feature_matrix <- structure(drug_feature_matrix,
+                                     datasets_included = match.arg(datasets_included, several.ok = TRUE))
+    return(drug_feature_matrix)
+  }
 
 show_metadata <- function(drug_feature_matrix) {
   # takes a drug_feature_matrix and shows its attributes except for 'class', 'row.names' and 'names'
-   stopifnot(is.data.frame(drug_feature_matrix))
    attrs <- attributes(drug_feature_matrix)
-   attrs[setdiff(names(attrs), c("class", "row.names", "names"))]
+   attrs[setdiff(names(attrs), c("class", "row.names", "names", "dim"))]
 }
