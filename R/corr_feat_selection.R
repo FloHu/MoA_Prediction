79819f5a4f882765e02b50fbd97c51df655fd4fe
Florian Huber <florian_huber@gmx.at>
2019-01-22 16:34:13 +0100

    Selection of dosage with highest interactions now done after selecting top x% variance features.
    Introduced merging of correlated features but based on hierarchical
    clustering rather than iterative merging. Threshold set to 0.7.

--- R/corr_feat_selection.R
+++ R/corr_feat_selection.R
@@ -15,17 +15,24 @@ melt_cormat_to_pairs <- function(cor_mat) {
   return(melted_cor_mat)
 }
 
-merge_features <- function(dfr, to_merge, f) {
+merge_features <- function(dfr, cluster_fr, f) {
   # takes a data frame and a list of features
   # each element of the list indicates which columns of the data frame should 
   # be merged using function f
-  stopifnot(all(unlist(to_merge) %in% colnames(dfr)))
-  map_dfc(to_merge, function(featureset) {
-    my_name <- paste(featureset, collapse = "_")
+  stopifnot(all(unlist(cluster_fr$members) %in% colnames(dfr)))
+  to_merge <- cluster_fr$members
+  names(to_merge) <- cluster_fr$clust_label
+  
+  newfeats <- imap_dfc(to_merge, function(featureset, label) {
     v <- tibble(apply(dfr[, featureset], 1, f))
-    names(v) <- my_name
+    names(v) <- label
     return(v)
   })
+  
+  message("Mapped ", length(unlist(to_merge)), " into ", nrow(cluster_fr), " derived features.")
+  dfr <- select(dfr, -one_of(unlist(to_merge)))
+  dfr <- bind_cols(dfr, newfeats)
+  return(dfr)
 }
 
 
--- R/data_preprocessing.R
+++ R/data_preprocessing.R
@@ -1,16 +1,20 @@
-feature_selection_variance <- function(data , featStart = 3, featStop = ncol(data), percentTop = 20){
-  #Filter features in a data frame based on their variance. Keep only the top n% (by default 20)
-  #Inputs
-  #Data : raw data frame
-  #featStart : allow function to skip some column that are no predictive features, like the outcome variable
-  allVar <- apply(data[ ,featStart:featStop], 2, var)
+feature_selection_variance <- function(data, 
+                                       percentTop, 
+                                       featStart = 3, 
+                                       featStop = ncol(data)) {
+  # Filter features in a data frame based on their variance. Keep only the top n%
+  # Inputs:
+  # data: raw data frame
+  # featStart  allow function to skip some column that are not predictive 
+  # features, like the outcome variable
+  allVar <- apply(data[, featStart:featStop], 2, var)
   allVar <- sort(allVar, decreasing = T)
   
-  allVar = allVar[1:(length(allVar)*(percentTop/100))]
+  allVar = allVar[1:(length(allVar) * (percentTop/100))]
   toKeep = c(names(data)[1:(featStart - 1)], names(allVar))
   # sanity check
   stopifnot(unique(toKeep) == toKeep)
-  return(data[ , toKeep])
+  return(data[, toKeep])
 }
 
 select_dosage_most_ias <- function(dfr) {
@@ -24,7 +28,48 @@ select_dosage_most_ias <- function(dfr) {
   return(dfr)
 }
 
+select_most_ias <- function(the_matrix, the_matrix_signifs) {
+  # purpose: select dosage with most interactions of the_matrix, if there are 
+  # ties, take the highest dosage
+  # INPUTS:
+  #  * the_matrix: a drug-feature matrix with several concentrations per drug
+  #  * the_matrix_signifs: a matrix containing at least the same drug-conc 
+  #    combinations of the_matrix, the same features, but indicating whether 
+  #    the interaction is significant or not
+  rowsa <- paste(the_matrix$drugname_typaslab, the_matrix$conc)
+  rowsb <- paste(the_matrix_signifs$drugname_typaslab, the_matrix_signifs$conc)
+  colsa <- colnames(the_matrix)
+  colsb <- colnames(the_matrix_signifs)
+  metavars <- c("drugname_typaslab", "conc")
+  
+  stopifnot({
+    all(colsa %in% colsb)
+    all(metavars %in% colnames(the_matrix))
+    all(rowsa %in% rowsb)
+    all(sapply(select(the_matrix_signifs, -one_of(metavars)), is.logical))
+  })
+  
+  the_matrix_signifs <- the_matrix_signifs[rowsb %in% rowsa, colsb %in% colsa]
+  the_matrix_signifs <- bind_cols(the_matrix_signifs[, metavars], 
+    nsignif = apply(select(the_matrix_signifs, -one_of(metavars)), 1, sum))
+  
+  winning_concs <- 
+    left_join(the_matrix[, metavars], the_matrix_signifs) %>%
+    group_by(drugname_typaslab) %>%
+    arrange(drugname_typaslab, desc(nsignif), desc(conc)) %>%
+    slice(1)
+  
+  the_matrix_subset <- semi_join(the_matrix, winning_concs) %>%
+    arrange(drugname_typaslab)
+  stopifnot({
+    ncol(the_matrix) == ncol(the_matrix_subset)
+    all(the_matrix$drugname_typaslab %in% the_matrix_subset$drugname_typaslab)
+  })
+  return(the_matrix_subset)
+}
+
 select_mutant <- function(dfr) {
+  # DEPRECATED
   if (is.null(dfr$strain)) {
     stop("Did not find column 'strain'")
   }
